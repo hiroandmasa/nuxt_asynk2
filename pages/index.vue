ae6ce209e3dca79cf7321a36ec1f7b5dccfad4e6
池田雅裕 <m-ikeda@sunseer.co.jp>
2020-05-04 18:12:05 +0900

    画像を追加しました。

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,14 @@
               {{ user.id }}, {{ user.name }}, {{ user.company.name }}
           </li>
       </ul>
+      <img src="~/assets/monster06.png">
     </div>
   </div>
 </template>
 
 <script>
 const axios = require('axios')
-let url = 'https://jsonplaceholder.typicode.com/usersxxx'
+let url = 'https://jsonplaceholder.typicode.com/users'
 
 export default {
     asyncData({ params, error}) {
